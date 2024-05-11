e5e6d164acd79edb4ff4774126f528e40edcf968
AlianBlank <wangfj11@foxmail.com>
2024-05-11 13:52:44 +0800

    [增加]1. 增加消息唯一编码的字段和相关函数

--- Runtime/Network/Base/MessageObject.cs
+++ Runtime/Network/Base/MessageObject.cs
@@ -1,6 +1,7 @@
 ﻿#if ENABLE_GAME_FRAME_X_PROTOBUF
 using ProtoBuf;
 #endif
+
 namespace GameFrameX.Network.Runtime
 {
     /// <summary>
@@ -11,5 +12,30 @@ namespace GameFrameX.Network.Runtime
 #endif
     public class MessageObject
     {
+        /// <summary>
+        /// 消息唯一编号
+        /// </summary>
+        public long UniqueId { get; private set; }
+
+        protected MessageObject()
+        {
+            UpdateUniqueId();
+        }
+
+        /// <summary>
+        /// 更新唯一编码
+        /// </summary>
+        public void UpdateUniqueId()
+        {
+            UniqueId = Utility.IdGenerator.GetNextUniqueId();
+        }
+
+        /// <summary>
+        /// 设置唯一编码
+        /// </summary>
+        public void SetUpdateUniqueId(long uniqueId)
+        {
+            UniqueId = uniqueId;
+        }
     }
 }
