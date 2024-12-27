9b584ab0729c3cece962ec562116ffc083c4de2c
Blank <wangfj11@foxmail.com>
2024-12-27 16:38:24 +0800

    [增加]1. 增加HTTP消息包基类

---
+++ Runtime/Network/Base/MessageHttpObject.cs
@@ -0,0 +1,44 @@
+﻿using GameFrameX.Runtime;
+using Newtonsoft.Json;
+#if ENABLE_GAME_FRAME_X_PROTOBUF
+using ProtoBuf;
+#endif
+
+namespace GameFrameX.Network.Runtime
+{
+    /// <summary>
+    /// HTTP消息包装基类
+    /// </summary>
+#if ENABLE_GAME_FRAME_X_PROTOBUF
+    [ProtoContract]
+#endif
+    public class MessageHttpObject
+    {
+        /// <summary>
+        /// 消息ID
+        /// </summary>
+#if ENABLE_GAME_FRAME_X_PROTOBUF
+        [ProtoMember(1)]
+#endif
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 消息序列号
+        /// </summary>
+#if ENABLE_GAME_FRAME_X_PROTOBUF
+        [ProtoMember(2)]
+#endif
+        public int UniqueId { get; set; }
+
+        [JsonIgnore]
+#if ENABLE_GAME_FRAME_X_PROTOBUF
+        [ProtoMember(3)]
+#endif
+        public byte[] Body { get; set; }
+
+        public override string ToString()
+        {
+            return Utility.Json.ToJson(this);
+        }
+    }
+}
