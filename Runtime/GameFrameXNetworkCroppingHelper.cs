ebe0882be594c42d2735778e6d2af7bc97f4d345
Blank <wangfj11@foxmail.com>
2025-02-21 10:34:58 +0800

    [增加]1. 增加程序集防裁剪引用

---
+++ Runtime/GameFrameXNetworkCroppingHelper.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace GameFrameX.Network.Runtime
+{
+    public class GameFrameXNetworkCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+            _ = typeof(GameFrameX.Network.Runtime.AddressFamily);
+            _ = typeof(GameFrameX.Network.Runtime.BasePacketHeartBeatHandler);
+            _ = typeof(GameFrameX.Network.Runtime.DefaultMessageCompressHandler);
+            _ = typeof(GameFrameX.Network.Runtime.DefaultMessageDecompressHandler);
+            _ = typeof(GameFrameX.Network.Runtime.DefaultNetworkChannelHelper);
+            _ = typeof(GameFrameX.Network.Runtime.DefaultPacketReceiveBodyHandler);
+            _ = typeof(GameFrameX.Network.Runtime.DefaultPacketReceiveHeaderHandler);
+            _ = typeof(GameFrameX.Network.Runtime.DefaultPacketSendBodyHandler);
+            _ = typeof(GameFrameX.Network.Runtime.DefaultPacketSendHeaderHandler);
+            _ = typeof(GameFrameX.Network.Runtime.GameFrameXNetworkCroppingHelper);
+            _ = typeof(GameFrameX.Network.Runtime.IHeartBeatMessage);
+            _ = typeof(GameFrameX.Network.Runtime.IMessageCompressHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IMessageDecompressHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IMessageHandler);
+            _ = typeof(GameFrameX.Network.Runtime.INetworkChannel);
+            _ = typeof(GameFrameX.Network.Runtime.INetworkChannelHelper);
+            _ = typeof(GameFrameX.Network.Runtime.INetworkManager);
+            _ = typeof(GameFrameX.Network.Runtime.INetworkSocket);
+            _ = typeof(GameFrameX.Network.Runtime.INotifyMessage);
+            _ = typeof(GameFrameX.Network.Runtime.IPacketHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IPacketHeartBeatHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IPacketReceiveBodyHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IPacketReceiveHeaderHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IPacketSendBodyHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IPacketSendHeaderHandler);
+            _ = typeof(GameFrameX.Network.Runtime.IRequestMessage);
+            _ = typeof(GameFrameX.Network.Runtime.IResponseMessage);
+            _ = typeof(GameFrameX.Network.Runtime.MessageHandlerAttribute);
+            _ = typeof(GameFrameX.Network.Runtime.MessageHttpObject);
+            _ = typeof(GameFrameX.Network.Runtime.MessageObject);
+            _ = typeof(GameFrameX.Network.Runtime.MessageTypeHandlerAttribute);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkClosedEventArgs);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkComponent);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkConnectedEventArgs);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkErrorCode);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkErrorEventArgs);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager.ConnectState);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager.HeartBeatState);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager.NetworkChannelBase);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager.ReceiveState);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager.RpcState);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager.RpcState.RpcMessageData);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkManager.SendState);
+            _ = typeof(GameFrameX.Network.Runtime.NetworkMissHeartBeatEventArgs);
+            _ = typeof(GameFrameX.Network.Runtime.ProtoMessageHandler);
+            _ = typeof(GameFrameX.Network.Runtime.ProtoMessageIdHandler);
+            _ = typeof(GameFrameX.Network.Runtime.ServiceType);
+        }
+    }
+}
